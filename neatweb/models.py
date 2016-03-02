4bd8576dc54db9bd67ef358b09fc93f94b80ce01
Jason Boutte <boutte.jason@gmail.com>
2016-03-02 02:25:28 -0800

    Adds initial models and migrations

--- neatweb/models.py
+++ neatweb/models.py
@@ -2,4 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Experiment(models.Model):
+    name = models.CharField(max_length=64)
+    config = models.TextField()
+
+class Population(models.Model):
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+
+class Generation(models.Model):
+    relative_index = models.IntegerField()
+    population = models.ForeignKey(Population, on_delete=models.CASCADE)
+
+class Species(models.Model):
+    avg_fitness = models.DecimalField(max_digits=20, decimal_places=16)
+    max_fitness = models.DecimalField(max_digits=20, decimal_places=16)
+    offspring = models.IntegerField()
+    age_since_imp = models.IntegerField()
+    population = models.ForeignKey(Population, on_delete=models.CASCADE)
+    generation = models.ForeignKey(Generation, on_delete=models.CASCADE)
+
+class Organism(models.Model):
+    fitness = models.DecimalField(max_digits=20, decimal_places=16)
+    rank = models.IntegerField()
